c4b201044a70eef9f84ca65bc2a1e606f5e4cf58
Ubuntu <ubuntu@ip-172-31-40-99.ap-northeast-1.compute.internal>
2022-04-25 02:34:04 +0000

    add session-number

--- traffic-mirror.sh
+++ traffic-mirror.sh
@@ -4,4 +4,4 @@ TARGETID=$1
 FILTERID=$2
 SOURCEENI=$3
 
-aws ec2 create-traffic-mirror-session --traffic-mirror-target-id ${TARGETID} --traffic-mirror-filter-id ${FILTERID} --network-interface-id ${SOURCEENI}
+aws ec2 create-traffic-mirror-session --traffic-mirror-target-id ${TARGETID} --traffic-mirror-filter-id ${FILTERID} --network-interface-id ${SOURCEENI} --session-number $(shuf -i 1-1000 -n 1)
